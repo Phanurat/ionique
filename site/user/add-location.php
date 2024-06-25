0a7c840ca6731a0796638cbcfd64e4aab5af1931
Panurat <paunavt5@gmail.com>
2024-06-25 10:08:42 +0700

    add user save address location

--- site/user/add-location.php
+++ site/user/add-location.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Leaflet with Geolocation and User Input</title>
+  <title>Add Location User</title>
   <!-- เรียกใช้ Leaflet ผ่าน CDN -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
   <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
@@ -103,6 +103,5 @@
       console.log('GPS is not available.');
     }
   </script>
-  <a href="user/add-location.php">User: Add location</a>
 </body>
 </html>
