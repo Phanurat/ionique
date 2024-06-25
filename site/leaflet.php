e749d9b1eaa6775d8814486ebe326f08388609bd
Panurat <paunavt5@gmail.com>
2024-06-25 09:43:10 +0700

    add scale

--- site/leaflet.php
+++ site/leaflet.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Leaflet with Geolocation and User Input</title>
   <!-- เรียกใช้ Leaflet ผ่าน CDN -->
   <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
